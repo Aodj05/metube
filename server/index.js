90ab3909cacde96d9b09605a10bf6d7166e354c4
Aodj05 <aodj05@msn.com>
2022-08-10 17:59:23 -0400

    setup mongoDB, models, routes, controllers

--- server/index.js
+++ server/index.js
@@ -2,6 +2,10 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/users.js";
+import commentsRoutes from "./routes/comments.js";
+import videosRoutes from "./routes/videos.js";
+import authRoutes from "./routes/auth.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 dotenv.config()
@@ -15,9 +19,26 @@ const connect = () => {
       .catch((err) => {
         throw err;
     });
-}
+};
+
+app.use(cookieParser());
+app.use(express.json());
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/comments", commentsRoutes);
+app.use("/api/videos", videosRoutes);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500;
+  const message = err.message || "Something went wrong";
+  return res.status(status).json({
+    success: false,
+    status,
+    message
+  });
+});
 
 app.listen(8800,() => {
     connect()
     console.log("Connected to Server");
-})
+});
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,28 @@
 import express from 'express';
-import { test } from "../controllers/user";
+import { deleteUser, dislike, getUser, like, subscribe, unsubscribe, update } from "../controllers/user.js";
+import { verifyToken } from '../verifyToken.js';
 
 const router = express.Router();
 
-router.get("/test", test);
+//Update user
+router.put("/:id", verifyToken, update);
+
+//Delete user
+router.delete("/:id", deleteUser);
+
+//Get user
+router.get("/find/:id", getUser);
+
+//Subscribe user
+router.put("/sub/:id", subscribe);
+
+//Unsubscribe user
+router.put("/unsub/:id", unsubscribe);
+
+//Like a video
+router.put("/like/:videoId", like);
+
+//Dislike a video
+router.put("/dislike/:videoId", dislike);
 
 export default router;
