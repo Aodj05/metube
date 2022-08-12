afe7af6198cc1379091e8c2f4afb717b14211f92
Aodj05 <aodj05@msn.com>
2022-08-12 18:37:05 -0400

    User sign in and sign up

---
+++ client/src/utils/Theme.js
@@ -0,0 +1,20 @@
+export const lightTheme = {
+    bg: "#004284",
+    bgLighter: "#004284",
+    text: "white",
+    textSoft: "#aaaaaa",
+    soft: "#3c77ab",
+    bgMain: "#d3e4f5",
+    title: "black",
+    textDesc: "black"
+}
+
+export const darkTheme = {
+    bg: "#004284",
+    bgLighter: "#004284",
+    text: "darkgrey",
+    textSoft: "black",
+    soft: "#154269",
+    bgMain: "#2b6cad",
+    title: "white"
+}
