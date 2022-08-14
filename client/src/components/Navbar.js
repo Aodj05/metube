df43602853d540fd55ffc4c4d8b53e4ec19004d0
Aodj05 <aodj05@msn.com>
2022-08-14 12:39:08 -0400

    search functionality and refactor

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import VideoCallOutlinedIcon from "@mui/icons-material/VideoCallOutlined";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import Upload from './Upload';
 
@@ -41,6 +41,7 @@ const Search = styled.div`
 const Input = styled.input`
   border: none;
   background-color: transparent;
+  outline: none;
 `;
 
 const Button = styled.button`
@@ -72,15 +73,20 @@ const Avatar = styled.img`
 `;
 
 const Navbar = () => {
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
+  const [q, setQ] = useState("");
   const { currentUser } = useSelector((state) => state.user);
     return(
       <>
         <Container>
             <Wrapper>
                 <Search>
-                    <Input placeholder='Search' />
-                    <SearchOutlinedIcon />
+                    <Input
+                      placeholder='Search'
+                      onChange={(e) => setQ(e.target.value)}
+                    />
+                    <SearchOutlinedIcon onClick={()=>navigate(`/search?q=${q}`)} />
                 </Search>
                 {currentUser ? (
                   <User>
